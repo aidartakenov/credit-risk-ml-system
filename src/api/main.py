5e9e8b198c6f08b04ff8f34a97663aba1a5bd7cd
Aidar <aidar.takenov@alatoo.edu.kg>
2026-03-14 16:14:42 +0600

    Created and connected to ui and database

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.schema import CreditRequest
 from src.services.prediction_service import predict
 
 app = FastAPI()
 
+# Добавляем CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # разрешаем все источники (для разработки)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Credit Risk API is running"}
 
 @app.post("/predict")
-def get_prediction(data: CreditRequest):
-    return predict(data)
+async def get_prediction(data: CreditRequest):
+    result = predict(data)
+    return result
